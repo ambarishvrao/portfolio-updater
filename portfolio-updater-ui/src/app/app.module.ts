7d1d6e71b09a094bb4fae86b1c1c3895d0487e87
Ambarish Rao <ambarish.r@turvo.com>
2023-04-02 22:18:20 +0530

    1. Add file upload component

--- portfolio-updater-ui/src/app/app.module.ts
+++ portfolio-updater-ui/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from
+	'@angular/common/http';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+	AppComponent,
+	FileUploadComponent,
+],
+imports: [
+	BrowserModule,
+	AppRoutingModule,
+	HttpClientModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
