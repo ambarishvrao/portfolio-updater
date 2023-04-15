96cf7e21ca75c6ac11aa76397cb3f204b816177b
Ambarish Rao <ambarish.r@turvo.com>
2023-04-15 21:52:04 +0530

    Add boilerplate python api code

--- casparser/app.py
+++ casparser/app.py
@@ -1,11 +1,41 @@
 import os
-from flask import Flask
+from flask import Flask, flash, redirect, request
 from flask import (url_for, current_app as app)
+from werkzeug.utils import secure_filename
+
+import casparser
+
 app = Flask(__name__)
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = {'pdf'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route("/parse", methods=['POST'])
+def parse_cas():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'])
+        file.save(upload_path, filename)
+        return casparser.read_cas_pdf(upload_path + filename, "password", output="json")
 
 
 # favicon
